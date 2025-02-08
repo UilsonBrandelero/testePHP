dad542c97f3b224e5520da8aa0ca3b4968093566
Uilson <uilson@uilson.com>
2025-02-08 17:00:19 -0300

    Adicionado metodo printDeuBoa

--- Classe.java
+++ Classe.java
@@ -5,4 +5,7 @@ public class Classe {
     public static void main(String[] args) {
         System.out.println("Deu Boa");
     }
+    public void printDeuBoa(){
+        System.out.println("Deu Boa");
+    }
 }
