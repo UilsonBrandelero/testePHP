8b4922cd632d1e4394260b53670c929087f9652a
Uilson <uilson@uilson.com>
2025-02-08 18:21:13 -0300

    criado branch.java

--- Classe.java
+++ Classe.java
@@ -5,7 +5,15 @@ public class Classe {
     public static void main(String[] args) {
         System.out.println("Deu Boa");
     }
+
     public void printDeuBoa(){
         System.out.println("Deu Boa");
     }
+    public  void printJava(){
+        System.out.println("Java");
+    }
+    public void printSpring(){
+        System.out.println("Spring");
+    }
+
 }
